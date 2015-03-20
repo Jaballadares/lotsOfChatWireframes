466098ca172611f929ce4aa9e6e8f9c748fffbc3
jaballadares <jballadares28@gmail.com>
2015-03-19 23:03:15 -0700

    this is a good start

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+
+$('.circles').on('click', function() {
+  // display the chatContainer
+   $('.chatContainer').removeClass('hide');
+   // replace circles with chatContainer
+   $(this).replaceWith($('.chatContainer'));
+
+});
+
 $('.clickSend').on('click', function() {
    $('.leftMessage').append($('.messageInput').val());
 	$('.messageInput').val("");
